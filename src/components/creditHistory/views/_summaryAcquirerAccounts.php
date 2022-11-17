0de980460bee272ab49ee28329e5350f189beeac
Glazkov M.O <miha.o,g@yandex.ru>
2022-11-17 18:40:06 +0300

    #1335

--- src/components/creditHistory/views/_summaryAcquirerAccounts.php
+++ src/components/creditHistory/views/_summaryAcquirerAccounts.php
@@ -1,6 +1,7 @@
 <?php
 /* @var $this View */
 /* @var $accountReply AccountReply[] */
+
 /* @var $accountReplyRUTDF AccountReplyRUTDF[] */
 
 use mfteam\nbch\models\AccountReply;
@@ -14,11 +15,13 @@ foreach ($accountReplyRUTDF as $model) {
     if (!empty($model->getAcquirerIndivid()) || !empty($model->getAcquirerLegal())) {
         $accountAmt = $model->getAccountAmt()[count($model->getAccountAmt()) - 1];
         $dueArrear = $model->getDueArrear()[count($model->getDueArrear()) - 1];
-        $pastdueArrear = $model->getPastdueArrear() ? $model->getPastdueArrear()[count($model->getPastdueArrear()) - 1] : null;
+        $pastdueArrear = $model->getPastdueArrear() ? $model->getPastdueArrear()[count(
+            $model->getPastdueArrear()
+        ) - 1] : null;
         $amtPastDueAccount = $pastdueArrear ? $pastdueArrear->amtPastDue : 0;
         $creditLimit += (float)$accountAmt->creditLimit;
         $amtOutstanding += (float)$dueArrear->amtOutstanding;
-        $amtPastDue += (float)$amtPastDueAccount->amtPastDue;
+        $amtPastDue += (float)$amtPastDueAccount;
         echo $this->render('_summaryAccountRUTDF', [
             'model' => $model,
         ]);
@@ -37,10 +40,13 @@ foreach ($accountReply as $account) {
 ?>
 <tr>
     <td colspan="3"><b>Итог:</b></td>
-    <td><?=Yii::$app->formatter->asDecimal($creditLimit, 2)?></td>
+    <td><?= Yii::$app->formatter->asDecimal($creditLimit, 2) ?></td>
     <td colspan="3"></td>
-    <td><?=Yii::$app->formatter->asDecimal($amtOutstanding, 2)?></td>
-    <td class="<?=$amtPastDue?'text-danger':'text-success'?>"><?=Yii::$app->formatter->asDecimal($amtPastDue, 2)?></td>
+    <td><?= Yii::$app->formatter->asDecimal($amtOutstanding, 2) ?></td>
+    <td class="<?= $amtPastDue ? 'text-danger' : 'text-success' ?>"><?= Yii::$app->formatter->asDecimal(
+            $amtPastDue,
+            2
+        ) ?></td>
     <td colspan="5"></td>
 </tr>
 
--- src/components/creditHistory/views/_summaryActiveAccounts.php
+++ src/components/creditHistory/views/_summaryActiveAccounts.php
@@ -8,14 +8,16 @@ $creditLimit = 0;
 $amtOutstanding = 0;
 $amtPastDue = 0;
 foreach ($accountReplyRUTDF as $model) {
-    $accountAmt = $model->getAccountAmt()[count($model->getAccountAmt()) - 1];
-    $dueArrear = $model->getDueArrear()[count($model->getDueArrear()) - 1];
-    $pastdueArrear = $model->getPastdueArrear() ? $model->getPastdueArrear()[count($model->getPastdueArrear()) - 1] : null;
-    $amtPastDueAccount = $pastdueArrear ? $pastdueArrear->amtPastDue : 0;
-    $creditLimit += (float)$accountAmt->creditLimit;
-    $amtOutstanding += (float)$dueArrear->amtOutstanding;
-    $amtPastDue += (float)$amtPastDueAccount->amtPastDue;
     if (empty($model->loanIndicator)) {
+        $accountAmt = $model->getAccountAmt()[count($model->getAccountAmt()) - 1];
+        $dueArrear = $model->getDueArrear()[count($model->getDueArrear()) - 1];
+        $pastdueArrear = $model->getPastdueArrear() ? $model->getPastdueArrear()[count(
+            $model->getPastdueArrear()
+        ) - 1] : null;
+        $amtPastDueAccount = $pastdueArrear ? $pastdueArrear->amtPastDue : 0;
+        $creditLimit += (float)$accountAmt->creditLimit;
+        $amtOutstanding += (float)$dueArrear->amtOutstanding;
+        $amtPastDue += (float)$amtPastDueAccount;
         echo $this->render('_summaryAccountRUTDF', [
             'model' => $model,
         ]);
@@ -34,10 +36,13 @@ foreach ($accountReply as $account) {
 ?>
 <tr>
     <td colspan="3"><b>Итог:</b></td>
-    <td><?=Yii::$app->formatter->asDecimal($creditLimit, 2)?></td>
+    <td><?= Yii::$app->formatter->asDecimal($creditLimit, 2) ?></td>
     <td colspan="3"></td>
-    <td><?=Yii::$app->formatter->asDecimal($amtOutstanding, 2)?></td>
-    <td class="<?=$amtPastDue?'text-danger':'text-success'?>"><?=Yii::$app->formatter->asDecimal($amtPastDue, 2)?></td>
+    <td><?= Yii::$app->formatter->asDecimal($amtOutstanding, 2) ?></td>
+    <td class="<?= $amtPastDue ? 'text-danger' : 'text-success' ?>"><?= Yii::$app->formatter->asDecimal(
+            $amtPastDue,
+            2
+        ) ?></td>
     <td colspan="5"></td>
 </tr>
 
