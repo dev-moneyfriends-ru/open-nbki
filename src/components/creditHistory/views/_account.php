d8e41a08c410aa3f46b5ab794bb38234ffb35a52
Glazkov M.O <miha.o,g@yandex.ru>
2022-11-17 19:46:19 +0300

    #1335

--- src/components/creditHistory/views/_account.php
+++ src/components/creditHistory/views/_account.php
@@ -8,213 +8,213 @@
         <h2>Информация по договору: <?=$model->uuid??$model->serialNum?></h2>
     </div>
 </div>
-<div class="row">
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Состояние договора</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >Состояние договора</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Специальный статус договора</div>
-            <div class="col-md-4" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-8 bg-info" >Специальный статус договора</div>
+            <div class="col-md-4" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Макс. сумма просроченной
+            <div class="col-md-6 bg-info" >Макс. сумма просроченной
                 задолженности</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
 </div>
-<div class="row">
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Дата перехода в состояние</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >Дата перехода в состояние</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Дата актуальности информации</div>
-            <div class="col-md-4" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-8 bg-info" >Дата актуальности информации</div>
+            <div class="col-md-4" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Тип источника информации</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >Тип источника информации</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
 </div>
-<div class="row">
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Уникальный
+            <div class="col-md-6 bg-info" >Уникальный
                 идентификатор договора</div>
             <div class="col-md-6"></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Вид отображения задолженности</div>
+            <div class="col-md-8 bg-info" >Вид отображения задолженности</div>
             <div class="col-md-4"></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Дата начала выгрузок в БКИ</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div >
+            <div class="col-md-6 bg-info" >Дата начала выгрузок в БКИ</div>
+            <div class="col-md-6" ></div >
         </div>
     </div>
 </div>
-<div class="row">
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">ID Договора</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >ID Договора</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Сумма погашенного основного долга</div>
-            <div class="col-md-4" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-8 bg-info" >Сумма погашенного основного долга</div>
+            <div class="col-md-4" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Полная стоимость кредита</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-4">
-        <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Отношение к договору</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Общая выплаченная сумма</div>
-            <div class="col-md-4" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Погашение за счет обеспечения</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >Полная стоимость кредита</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
 </div>
-<div class="row">
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Тип договора</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >Отношение к договору</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Сумма последнего платежа</div>
-            <div class="col-md-4" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-8 bg-info" >Общая выплаченная сумма</div>
+            <div class="col-md-4" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Аннуитет</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-4">
-        <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Дата начала договора</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Дата последнего платежа</div>
-            <div class="col-md-4" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Дата уплаты процентов</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >Погашение за счет обеспечения</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
 </div>
-<div class="row">
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Дата окончания договора</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >Тип договора</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Текущая задолженность</div>
-            <div class="col-md-4" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-8 bg-info" >Сумма последнего платежа</div>
+            <div class="col-md-4" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Количество пролонгаций</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-4">
-        <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Сумма обязательств по договору</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Текущий неиспользованный лимит</div>
-            <div class="col-md-4" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="row" style="height: 100%">
-            <div class="col-md-12 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Количество просрочек</div>
+            <div class="col-md-6 bg-info" >Аннуитет</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
 </div>
-<div class="row">
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Первоначальный лимит</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >Дата начала договора</div>
+            <div class="col-md-6" ></div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row" style="height: 100%">
+            <div class="col-md-8 bg-info" >Дата последнего платежа</div>
+            <div class="col-md-4" ></div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row" style="height: 100%">
+            <div class="col-md-6 bg-info" >Дата уплаты процентов</div>
+            <div class="col-md-6" ></div>
+        </div>
+    </div>
+</div>
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
+    <div class="col-md-4">
+        <div class="row" style="height: 100%">
+            <div class="col-md-6 bg-info" >Дата окончания договора</div>
+            <div class="col-md-6" ></div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row" style="height: 100%">
+            <div class="col-md-8 bg-info" >Текущая задолженность</div>
+            <div class="col-md-4" ></div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row" style="height: 100%">
+            <div class="col-md-6 bg-info" >Количество пролонгаций</div>
+            <div class="col-md-6" ></div>
+        </div>
+    </div>
+</div>
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
+    <div class="col-md-4">
+        <div class="row" style="height: 100%">
+            <div class="col-md-6 bg-info" >Сумма обязательств по договору</div>
+            <div class="col-md-6" ></div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row" style="height: 100%">
+            <div class="col-md-8 bg-info" >Текущий неиспользованный лимит</div>
+            <div class="col-md-4" ></div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row" style="height: 100%">
+            <div class="col-md-12 bg-info" >Количество просрочек</div>
+        </div>
+    </div>
+</div>
+<div class="row" style="margin-bottom: 10px;border-bottom: 1px solid #000000;border-top: 1px solid #000000;">
+    <div class="col-md-4">
+        <div class="row" style="height: 100%">
+            <div class="col-md-6 bg-info" >Первоначальный лимит</div>
+            <div class="col-md-6" ></div>
         </div>
         <div class="row">
-            <div class="col-md-6 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Статус кредитной линии</div>
-            <div class="col-md-6" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-6 bg-info" >Статус кредитной линии</div>
+            <div class="col-md-6" ></div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="row" style="height: 100%">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Текущая просроченная задолженность</div>
-            <div class="col-md-4" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;"></div>
+            <div class="col-md-8 bg-info" >Текущая просроченная задолженность</div>
+            <div class="col-md-4" ></div>
         </div>
         <div class="row">
-            <div class="col-md-8 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">Текущая просроченная задолженность, дней</div>
+            <div class="col-md-8 bg-info" >Текущая просроченная задолженность, дней</div>
             <div class="col-md-4"></div>
         </div>
     </div>
     <div class="col-md-4">
-        <div class="row" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">
-            <div class="col-md-4 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">от 30 до 59 дней</div>
-            <div class="col-md-4 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">от 60 до 89 дней</div>
-            <div class="col-md-4 bg-info" style="height: 100%; border-bottom: 1px solid #000000; margin-bottom: 5px;">более 90 дней</div>
+        <div class="row" >
+            <div class="col-md-4 bg-info" >от 30 до 59 дней</div>
+            <div class="col-md-4 bg-info" >от 60 до 89 дней</div>
+            <div class="col-md-4 bg-info" >более 90 дней</div>
             <div class="col-md-4"></div>
             <div class="col-md-4"></div>
             <div class="col-md-4"></div>
