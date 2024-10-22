ea7c4d5bbdabf792d18d870dd6e552e9f05fcaf4
Glazkov M.O. <miha.o,g@yandex.ru>
2024-10-22 18:05:44 +0300

    fix OKSM

--- src/models/IdReq.php
+++ src/models/IdReq.php
@@ -4,5 +4,17 @@ namespace mfteam\nbch\models;
 
 class IdReq extends Identification
 {
-    
+    public function fields(): array
+    {
+        return [
+            'idNum',
+            'idType',
+            'idTypeText',
+            'seriesNumber',
+            'issueDate',
+            'OKSM',
+            'issueAuthority',
+            'divCode',
+        ];
+    }
 }
